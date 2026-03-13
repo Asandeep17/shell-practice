85cf9ec0744aabeba0bca371e68d2b1e72c20552
SandeepIT1 <sandeep1701sa@gmail.com>
2026-03-13 17:23:58 +0530

    actions

---
+++ 03-variables.sh
@@ -0,0 +1,11 @@
+
+PERSON1=Trump # no space between value and=
+PERSON2=MODI
+
+echo "$PERSON1:: Hello $PERSON2, How are You?"
+echo "$PERSON2:: Hi $PERSON1, I am fine Thanks for asking.how are you doing"
+echo "$PERSON1:: doing fine. what are you laraning $PERSON2?"
+echo "$PERSON2:: I am learning shell script.what about you?"
+echo "$PERSON1:: ok Bye"
+echo "$PERSON2:: ok Bye"
+echo "$PERSON1:: see you later"
