bcaf0eeaec0c0d5d640d612751ca531afe3f3970
SandeepIT1 <sandeep1701sa@gmail.com>
2026-03-13 15:11:41 +0530

    actions

---
+++ 02-conversation.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "suresh:: Hello Ramesh, How are You?"
+echo "Ramesh:: Hi Suresh, I am fine Thanks for asking.how are you doing"
+echo "Suresh:: doing fine. what are you laraning Ramesh?"
+echo "ramesh:: I am learning shell script.what about you?"
