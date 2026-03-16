a042fd255edd867708040e740ccad73b59d5d750
SandeepIT1 <sandeep1701sa@gmail.com>
2026-03-16 12:00:34 +0530

    acti

--- 06-variable.sh
+++ 06-variable.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 
-START_TIME=$(date)
+START_TIME=$(date +%s)
  
   echo "script executed at: $START_TIME"
   sleep 10
 
-  ENDTIME=$(date)
+  ENDTIME=$(date +%s)
+
+  TOTAL_TIME=$($ENDTIME-$START_TIME)
+
+  echo "script executed in :$TOTAL_TIME"
