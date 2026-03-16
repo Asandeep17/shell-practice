0e4b9c0b340b5bae1435c0093cf6abaeb0527977
SandeepIT1 <sandeep1701sa@gmail.com>
2026-03-16 12:09:43 +0530

    acti

--- 06-variable.sh
+++ 06-variable.sh
@@ -8,6 +8,7 @@ START_TIME=$(date +%s)
 
   END_TIME=$(date +%s)
 
-  TOTAL_TIME=$(($ENDTIME_$START_TIME))
+  TOTAL_TIME=$(($END_TIME-$START_TIME))
+  
 
   echo "script executed in :$TOTAL_TIME seconds"
