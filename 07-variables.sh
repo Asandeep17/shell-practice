9e3f48bcb4d2df7fa71906ee1aa00b07a761dda7
SandeepIT1 <sandeep1701sa@gmail.com>
2026-03-18 06:52:10 +0530

    acti

---
+++ 07-variables.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+### special variables###
+echo "All argus passes to script : $@"
+echo "Numbers of vars passesed to script: $#"
+echo "script name : $0"
+echo "present directory: $PWD"
+echo "who is running : $USER"
+echo "home directory of current user : $HOME"
+echo "pid of this Script: $$"
+sleep 100 &
+echo "PID of recently executed background process: $!"
+echo "all grgs passed to script: $*"cscd repos 
