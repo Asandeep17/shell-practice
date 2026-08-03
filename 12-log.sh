8abbcc9776ba786a8eb782ac38b5536968140c1b
SandeepIT1 <sandeep1701sa@gmail.com>
2026-08-03 16:49:54 +0530

    actions

--- 12-log.sh
+++ 12-log.sh
@@ -11,10 +11,10 @@ echo "please run this script with root user access"
 exit 1
 fi
 mkdir -p $LOGS_FOLDER
-VAlIDATE(){
+VALIDATE(){
     if [$1 -ne 0 ];then 
     echo "$2 ... FAILURE"
-    exit1 
+    exit 1 
     else 
         echo "$2 ... success"
 
