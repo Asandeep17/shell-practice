585cedd6d52810d69e09106a5a131494d8b96a7e
SandeepIT1 <sandeep1701sa@gmail.com>
2026-08-05 18:45:08 +0530

    actions

---
+++ 14-loop.sh
@@ -0,0 +1,33 @@
+
+#!/bin/bash
+
+
+USERID=$(id -u)
+
+LOGS_FOLDER="/var/log/shell-script"
+LOGS_FILE="/var/log/shell-script/$0.log"
+
+if [ $USERID -ne 0 ]; then 
+echo "please run this script with root user access" | tee -a $LOG_FILE
+exit 1
+
+fi
+
+mkdir -p $LOGS_FOLDER
+VALIDATE(){
+    if [ $1 -ne 0 ]; then 
+    echo "$2 ... FAILURE" |tee -a $LOG_FILE
+    exit 1 
+    else 
+        echo "$2 ... SUCCESS" |tee -a $LOG_FILE
+        fi
+
+}
+
+
+for package in $@ # sudo sh 14-loop.sh ngnix mysql nodejs
+ do 
+   dnf install $package -y
+   VALIDATE $? "$package installation "
+
+done   
