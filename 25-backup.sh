94f59e08b6df151cc09c65311bf627b1299aea3b
SandeepIT1 <sandeep1701sa@gmail.com>
2026-09-10 16:46:24 +0530

    actions

--- 25-backup.sh
+++ 25-backup.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 USERID=$(id -u)
-
 LOGS_FOLDER="/var/log/shell-script"
 LOGS_FILE="/var/log/shell-script/backup.log"
 R="\e[31m"
@@ -10,21 +9,19 @@ Y="\e[33m"
 N="\e[0m"
 SOURCE_DIR=$1
 DEST_DIR=$2
-
 DAYS=${3:-14} #14 days is the default value if the user not supplied
 
 if [ $USERID -ne 0 ]; then 
 echo -e " $R please run this script with root user access $N" | tee -a $LOG_FILE
-exit 1
-
 fi
 
 mkdir -p $LOGS_FOLDER
-USAGE(){
 
-    echo -e "$R USAGE::sudo backup <SOURCE_DIR> DEST_DIR> DAYS[deafault 14 days]"
+USAGE(){
+     echo -e "$R USAGE::sudo backup <SOURCE_DIR> DEST_DIR> DAYS[deafault 14 days]"
     exit 1
 }
+ 
 if [ $# -lt 2 ] ; then
 USAGE
 fi
@@ -38,4 +35,5 @@ fi
 
 if [ ! -d $DOURCE_DIR] ; then 
 echo "$R $DIST_DIR does not exitst $N"
+exit 1
 fi
