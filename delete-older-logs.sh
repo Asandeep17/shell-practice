d0bcd1b13db7533b7a14d358895d33ee1863bd56
SandeepIT1 <sandeep1701sa@gmail.com>
2026-09-10 13:27:57 +0530

    actions

--- delete-older-logs.sh
+++ delete-older-logs.sh
@@ -11,8 +11,6 @@ LOGS_FILE="$LOGS_DIR/$0.log"
 if [ ! -d $LOGS_DIR ]: then
    echo -e "$LOGS_DIR does not exitst"
    exit 1
-   
-   
 fi
 
 FILES_TO_DELETE=$(find $LOGS_DIR -name "*.log" -mtime +14)
